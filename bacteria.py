5cedae0f668fa7a6889a13d126f6a85e68bf9f57
danilom5 <martinezcastilloda@gmail.com>
2025-06-15 13:53:34 -0400

    Add files via upload

---
+++ bacteria.py
@@ -0,0 +1,7 @@
+# Bacteria
+#id: identificador ´unico.
+#raza: identificador gen´etico o especie.
+#energia: nivel de energ´ıa actual.
+#resistente: booleano que indica si es resistente.
+#estado: activa o muerta.
+#M´etodos: alimentar(), dividirse(), mutar(), morir().
