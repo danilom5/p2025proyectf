a6863707c3a464bf94cfd3199fbc76ffbd0360d0
danilom5 <martinezcastilloda@gmail.com>
2025-06-22 12:05:44 -0400

    Add files via upload
    añadi los parametros al constructor de bacteria, aparte de los que pide la pauta, añadi fila y columna para poder identificar la posicion de cada una

--- bacteria.py
+++ bacteria.py
@@ -1,18 +1,23 @@
-# Bacteria
-#id: identificador ´unico.
-#raza: identificador gen´etico o especie.
-#energia: nivel de energ´ıa actual.
-#resistente: booleano que indica si es resistente.
-#estado: activa o muerta.
-#M´etodos: alimentar(), dividirse(), mutar(), morir().
-
 class Bacteria:
-    def __init__(self, id_bacteria, raza, energia, resistente=False):
+    def __init__(self, id_bacteria, raza, energia, fila, columna, resistente=False):
+        """
+        Constructor de la clase Bacteria.
+        - id_bacteria: Identificador unico para rastrear la bacteria (ej: b1,b2, o cualquier otro, para diferenciarse de una en otra).
+        - raza: Representa el tipo, A o B o C etc
+        - energia: Energía inicial que tendrá la bacteria.
+        - fila, columna: Posición en la grilla donde vive la bacteria
+        - resistente: Booleano que indica si tiene resistencia a factores ambientales como los antibioticos
+        """
         self.id = id_bacteria
         self.raza = raza
         self.energia = energia
         self.resistente = resistente
-        self.estado = "activa"  # o "muerta"
+        self.estado = "activa" # Estado vital: activa o muerta
+
+        # Ubicación espacial en la grilla del ambiente
+        self.fila = fila
+        self.columna = columna
+
 
     def alimentar(self):
         pass
@@ -25,3 +30,4 @@ class Bacteria:
 
     def morir(self):
         pass
+        #energias random
