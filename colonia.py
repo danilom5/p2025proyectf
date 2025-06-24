1f272599aa76b214960691007bffdd3aea7a6fb0
danilom5 <martinezcastilloda@gmail.com>
2025-06-24 17:13:25 -0400

    metodo paso en colonia
    todas las bacterias vivas se alimentan. si no logran suficiente energía, pueden morir, y si tienen suficiente energía, pueden dividirse. evito errores al recorrer una copia de la lista original, ya que algunas bacterias pueden modificarla al dividirse, y se bugea

--- colonia.py
+++ colonia.py
@@ -8,14 +8,32 @@ from bacteria import Bacteria
 
 class Colonia:
     def __init__(self, ambiente):
-        self.bacterias = []  # lista de Bacteria
+        """
+        constructor de la colonia.
+        - recibe el ambiente en el que vive.
+        - tendra una lista de bacterias que viven en ese ambiente.
+        """
         self.ambiente = ambiente
+        self.bacterias = []  # Esta lista se completa desde Ambiente, es donde se guardan todas las bacterias de esta colonia
 
     def paso(self):
-        pass
+        """
+        simula un paso de tiempo en la colonia.
+        cada bacteria se alimenta, puede morir por inanicion, y puede dividirse.
+        """
 
-    def reporte_estado(self):
-        pass
+        # copia de la lista original para evitar errores al modificarla mientras iteramos
+        bacterias_activas = self.bacterias.copy()
 
-    def exportar_csv(self, nombre_archivo):
-        pass
+        for bacteria in bacterias_activas: 
+            if bacteria.estado == "activa": #verificar si esta viva
+                # Paso 1: se alimenta
+                bacteria.alimentar(self.ambiente) #llamamos el metodo alimentar 
+
+                # Paso 2: si sobrevive, puede dividirse
+                if bacteria.estado == "activa":
+                    bacteria.dividirse(self.ambiente, self.bacterias) #solo se divide si sigue viva 
+
+
+
+
